3000af8f8890474a6d1ee16c2bf9e4cccd1c518f
Dinhkhoi2000 <vokhoa170920@gmail.com>
2022-03-26 22:14:43 +0700

    make it better

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/EmployeeDB";
-//const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
+//const url = "mongodb://localhost:27017/EmployeeDB";
+const url = "mongodb+srv://dinhkhoi:fYFIKFNk62bonuXc@cluster0.rzqty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
