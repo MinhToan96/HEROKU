76af02e8826a64da2c9261b0388fa3ce9081871a
MinhToan <nguyenmingtoan508@gmail.com>
2022-03-28 14:47:40 +0700

    make it better

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
-const url = "mongodb+srv://khoi:z3cwQNFiyuxcNvrT@cluster0.rzqty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const url = "mongodb+srv://minhtoan:UoRW8uVoLZVr2LbJ@cluster0.1oden.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
